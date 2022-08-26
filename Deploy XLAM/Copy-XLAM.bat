e64bd6232d75527702a71d321540debd1e1f9bcc
bendunne <29761558+bendunne@users.noreply.github.com>
2022-08-26 13:16:24 +0100

    Revert "Initial Release of Deploy XLAM"
    This reverts commit fe66fc95f44a9a7ac9a0cb1e50f2c254bf180ddb.

--- Deploy XLAM/Copy-XLAM.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-pushd %~dp0
-PowerShell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -Command "& '.\Copy-XLAM.ps1'"
-popd
--- Deploy XLAM/Remove-XLAM.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-pushd %~dp0
-PowerShell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -Command "& '.\Remove-XLAM.ps1'"
-popd
